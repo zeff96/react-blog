931d3ec80729b4c589938ff65ad6449fdf4d48f6
zeff96 <zeffo1975@gmail.com>
2023-08-22 09:27:43 +0300

    initial commit

---
+++ app/javascript/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+
+const App = () => {
+  return(
+    <Router>
+      <Routes>
+        <Route exact path="/" element={ <Homepage /> } />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
---
+++ app/javascript/src/components/Homepage.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Homepage.css"
+const Homepage = () => {
+  return(
+    <h2 className="text">test test</h2>
+  )
+}
+
+export default Homepage
