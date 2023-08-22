0ba60a9b919dc516dd1d5254474eaeebe063b981
zeff96 <zeffo1975@gmail.com>
2023-08-22 11:52:04 +0300

    Update routes for user registration, authentication and passwords management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "root#index"
+
+  post '/signup', to: "registrations#create"
+  post '/login', to: "authentication#create"
+  post '/reset_password', to: "passwords#reset_password"
+  put '/update_password', to: "passwords#update_password"
 end
