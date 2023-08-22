5030c8be95395bd9a796174b570ab32267d3f2c5
zeff96 <zeffo1975@gmail.com>
2023-08-22 11:38:39 +0300

    Add password_reset_genaration mechanism

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def generate_password_reset_token
+    self.password_reset_token = SecureRandom.urlsafe.base64
+    self.password_reset_token_expires_at = Time.now + 1.hour
+  end
 end
