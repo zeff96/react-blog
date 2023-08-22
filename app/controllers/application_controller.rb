80e5f04b5edcd2beaaff49923e7134876c40d199
zeff96 <zeffo1975@gmail.com>
2023-08-22 11:48:58 +0300

    Add token decodind and authorization mechanism

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  attr_reader :current_user
+  before_action :authenticate_token!
+
+  private
+
+  def authenticate_token!
+    payload = JsonWebtoken.decode(auth_token)
+    @current_user = User.find(payload["sub"])
+  rescue JWT::ExpiredSignature
+    render json: { error: "Auth token has expired" }, status: :unauthorized
+  rescue JWT::DecodeError
+    render json: { error: "Invalid auth token" }, status: :unauthorized
+  end
+
+  def auth_token
+    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
+  end
 end
