9ed9bb491c3dda202ad505844a1d7a81238d3f3d
zeff96 <zeffo1975@gmail.com>
2023-08-22 11:05:37 +0300

    Add user password update mechanism

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -9,4 +9,22 @@ class PasswordsController < ApplicationController
       render json: { error: "User not found!" }, status: :not_found
     end
   end
+
+  def update_password
+    user = User.find_by(password_reset_token: params[:user][:password_reset_token])
+
+    if user && password_reset_token_valid?
+      user.update(
+        password: params[:user][:password],
+        password_confirmation: params[:user][:password_confirmation],
+        password_reset_token: nil,
+        password_reset_token_expires_at: nil
+      )
+
+      render json: { message: "Password reset successfull" }, status: :ok
+
+    else
+      render json: { error: "Invalid token or failed update" }, status: :unauthorized
+    end
+  end
 end
