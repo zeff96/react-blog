5ff4e285e6f10ad4bd19ccd5cc9c0e6fce6923e8
zeff96 <zeffo1975@gmail.com>
2023-08-23 07:53:15 +0300

    Refactor usermailer controllers to remove mailer url from it

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def password_reset(user)
     @user = user
-    @url = "http://localhost:3000/reset_password?password_reset_token=#{@user.password_reset_token}"
     mail(to: @user.email, subject: "Password reset instructions")
   end
 end
