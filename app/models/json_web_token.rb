3ee2e9924627b67d07c15966a293be02c8489ede
zeff96 <zeffo1975@gmail.com>
2023-08-24 18:11:59 +0300

    Update routes typo error

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,7 +1,6 @@
 class JsonWebToken
   def self.encode(payload)
-    expiration = 10.minutes.from_now.to_i
-    JWT.encode(payload.merge(exp: expiration), ENV["JWT_SECRET_KEY"], "HS256")
+    JWT.encode(payload, ENV["JWT_SECRET_KEY"], "HS256")
   end
 
   def self.decode(token)
