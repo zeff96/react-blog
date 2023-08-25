929f1f500d55d52c1ea5cb553c721fcd24106c72
zeff96 <zeffo1975@gmail.com>
2023-08-25 13:27:26 +0300

    Add realative paths for authentication and registration

--- app/javascript/src/components/Homepage.js
+++ app/javascript/src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from "react"
 import "./Homepage.css"
+import './authentication/Authentication';
+import './authentication/Registration';
 const Homepage = () => {
   return(
     <h2 className="text">test test</h2>
