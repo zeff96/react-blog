9dc8d11e0423b3a9b38f231d41cdef248ce3a726
zeff96 <zeffo1975@gmail.com>
2023-09-14 06:32:02 +0300

    Add background color to navbar

--- app/javascript/src/components/Navbar.js
+++ app/javascript/src/components/Navbar.js
@@ -5,32 +5,30 @@ import './Navbar.css';
 const NavbarComponet = () => {
   return (
     <nav className='nav'>
-      <div className='container'>
-        <NavLink className='nav-link-home'>Blog App</NavLink>
-        <button className='navbar-toggler'>
-          <div className='hamburger'></div>
-          <div className='hamburger'></div>
-          <div className='hamburger'></div>
-        </button>
-        <div>
-          <ul className='nav-items'>
-            <li>
-              <NavLink to='/' className='nav-link'>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/registration' className='nav-link'>
-                sign Up
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className='nav-link ' to={'/login'}>
-                Login
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+      <NavLink className='nav-link-home'>Blog App</NavLink>
+      <button className='navbar-toggler'>
+        <div className='hamburger'></div>
+        <div className='hamburger'></div>
+        <div className='hamburger'></div>
+      </button>
+      <div>
+        <ul className='nav-items'>
+          <li>
+            <NavLink to='/' className='nav-link'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/registration' className='nav-link'>
+              sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-link ' to={'/login'}>
+              Login
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
