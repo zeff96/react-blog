2950c789ef5574e4ca46e82b60e3e9c8a887b958
zeff96 <zeffo1975@gmail.com>
2023-09-14 07:32:07 +0300

    Render all components within the Layout component

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Regsitration from './components/authentication/Registration';
 import Authentication from './components/authentication/Authentication';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Homepage />}>
+        <Route element={<Layout />}>
+          <Route exact path='/' element={<Homepage />} />
           <Route exact path='/registration' element={<Regsitration />} />
           <Route exact path='/login' element={<Authentication />} />
         </Route>
