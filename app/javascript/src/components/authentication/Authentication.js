9896a3bf7359bb7078171a1bd1b67f686d8caa3d
zeff96 <zeffo1975@gmail.com>
2023-09-20 19:14:39 +0300

    Add class form-label to authentication label element

--- app/javascript/src/components/authentication/Authentication.js
+++ app/javascript/src/components/authentication/Authentication.js
@@ -1,31 +1,46 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 
 const Authentication = () => {
   const formRef = useRef();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const formData = new FormData(formRef.current);
     const form = Object.fromEntries(formData);
 
     const data = {
-      user: { email: form.email, password: form.password}
-    }
-  }
-  return(
+      user: { email: form.email, password: form.password },
+    };
+  };
+  return (
     <form ref={formRef}>
-      <label htmlFor="email">
+      <label htmlFor='email' className='form-label'>
         Email
-        <input type="email" name="email" placeholder="Enter email" autoComplete="email" autoFocus required className="form-control" />
+        <input
+          type='email'
+          name='email'
+          placeholder='Enter email'
+          autoComplete='email'
+          autoFocus
+          required
+          className='form-control'
+        />
       </label>
-      <label htmlFor="password">
+      <label htmlFor='password' className='form-label'>
         Password
-        <input type="password" name="password" placeholder="Enter password" autoFocus required className="form-control" />
+        <input
+          type='password'
+          name='password'
+          placeholder='Enter password'
+          autoFocus
+          required
+          className='form-control'
+        />
       </label>
-      <input type="submit" value="Log in" className="btn" />
+      <input type='submit' value='Log in' className='btn' />
     </form>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
