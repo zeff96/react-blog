40c2712e0eeb6d8df1febcaa1bf11a8fc98bfeeb
zeff96 <zeffo1975@gmail.com>
2023-09-22 11:10:14 +0300

    Add fix for route error

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
   get '/reset_password/edit', to: "passwords#edit"
   put '/update_password', to: "passwords#update"
 
-  namespace do
-    api do
-      v1 do
-        resources :posts
-      end
+  namespace :api do
+    namespace :v1 do
+      resources :posts
     end
   end
 end
