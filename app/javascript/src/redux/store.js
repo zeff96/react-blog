b3376247ad494cbecad48f28c51dffc15a561702
zeff96 <zeffo1975@gmail.com>
2023-10-03 12:25:14 +0300

    Add store instance and pass rootReducer instance to it

--- app/javascript/src/redux/store.js
+++ app/javascript/src/redux/store.js
@@ -1,3 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({});
+
+const store = (preloadedState) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export default store;
