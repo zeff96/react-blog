762d807905b388933b92a8aa342a986bf40ad74b
zeff96 <zeffo1975@gmail.com>
2023-10-03 12:42:10 +0300

    Add apiSlice for fetching and post data to api endpoints

---
+++ app/javascript/src/redux/api/apiSlice.js
@@ -0,0 +1,6 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/api/v1' }),
+});
