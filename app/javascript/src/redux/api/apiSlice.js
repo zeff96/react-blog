a90cab875ac13558fa8565702647cffe45d9d63f
zeff96 <zeffo1975@gmail.com>
2023-10-03 12:49:14 +0300

    Add api endpoint for getting all users

--- app/javascript/src/redux/api/apiSlice.js
+++ app/javascript/src/redux/api/apiSlice.js
@@ -3,4 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: '/api/v1' }),
+  tagTypes: ['users'],
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => '/users',
+    }),
+  }),
 });
