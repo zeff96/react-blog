2be5814f63575ea719c1e2a99ad0adab75c910e9
zeff96 <zeffo1975@gmail.com>
2023-10-04 16:56:51 +0300

    Add routes update for user authentication, registration and passwords

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   root "root#index"
   get '*path', to: 'root#index', constraints: ->(request) { !request.xhr? && request.format.html? }
 
-  post '/signup', to: "registrations#create"
-  post '/login', to: "authentication#create"
-  post '/reset_password', to: "passwords#create"
-  get '/reset_password/edit', to: "passwords#edit"
-  put '/update_password', to: "passwords#update"
-
   namespace :api do
     namespace :v1 do
       resources :posts
       resources :users, only: [:index]
+
+      post '/signup', to: "registrations#create"
+      post '/login', to: "authentication#create"
+      post '/reset_password', to: "passwords#create"
+      get '/reset_password/edit', to: "passwords#edit"
+      put '/update_password', to: "passwords#update"
     end
   end
 end
