52929a4f4aef69149320853dd007882cc90c456d
zeff96 <zeffo1975@gmail.com>
2023-10-05 17:39:22 +0300

    Add typo fix for minor bugs on comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,8 @@
-class Api::V1::CommentsController < ApplicationRecord
+class Api::V1::CommentsController < ApplicationController
   before_action :set_post, only: %i[new create destroy]
   before_action :set_comment, only: [:destroy]
-  
-  def 
+
+  def new
     @comment = @post.comments.build
     @comment.user = current_user
 
@@ -16,13 +16,13 @@ class Api::V1::CommentsController < ApplicationRecord
     if @comment.save
       render json: {comment: @comment, message: 'Commented created successfully'}, status: :created
     else
-      render json: {error: @comment.errors.full_message}, status: :unprocessable_entity
+      render json: {error: @comment.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def destroy
     @comment.destroy
-    render head: no_content
+    head: no_content
   end
 
   private
@@ -36,6 +36,6 @@ class Api::V1::CommentsController < ApplicationRecord
   end
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
   end
 end
