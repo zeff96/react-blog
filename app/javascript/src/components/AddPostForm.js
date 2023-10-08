570c3bc9296c99dad6e1357e5eecc77da944c9fb
zeff96 <zeffo1975@gmail.com>
2023-10-08 18:47:58 +0300

    Update font size and font family to addpostform component

--- app/javascript/src/components/AddPostForm.js
+++ app/javascript/src/components/AddPostForm.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import styles from './AddPostForm.css';
+import './AddPostForm.css';
 
 const AddPostForm = () => {
   return (
-    <form action=''>
-      <label htmlFor='title'>
+    <form action='' className='form'>
+      <label htmlFor='title' className='form-label'>
         Title
-        <input type='text' name='title' id='title' placeholder='Post title' />
+        <input
+          type='text'
+          name='title'
+          id='title'
+          placeholder='Post title'
+          className='form-control'
+        />
       </label>
-      <label htmlFor='content'>
+      <label htmlFor='content' className='form-label'>
         Content
         <textarea
           name='content'
           id='content'
           placeholder='Post body'
+          className='form-control'
         ></textarea>
       </label>
-      <input type='submit' value='Create Post' />
+      <input type='submit' value='Create Post' className='btn' />
     </form>
   );
 };
