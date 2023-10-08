20260a13f5b7ec5ca8efde34bf5a59d3d92d5d8c
zeff96 <zeffo1975@gmail.com>
2023-10-08 18:25:25 +0300

    Add input field for post title

--- app/javascript/src/components/AddPostForm.js
+++ app/javascript/src/components/AddPostForm.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const AddPostForm = () => {
-  return <div></div>;
+  return (
+    <form action=''>
+      <label htmlFor='title'>
+        Title
+        <input type='text' name='title' id='title' placeholder='Post title' />
+      </label>
+    </form>
+  );
 };
 
 export default AddPostForm;
