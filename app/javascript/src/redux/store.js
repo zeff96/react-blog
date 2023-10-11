26ff2fd9137598921ce54b7f47326c0fcc3917d5
zeff96 <zeffo1975@gmail.com>
2023-10-11 04:26:33 +0300

    Add authReducer slice to redux store

--- app/javascript/src/redux/store.js
+++ app/javascript/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
+import authReducer from './features/authSlice';
 
 export default configureStore({
   reducer: {
+    auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
