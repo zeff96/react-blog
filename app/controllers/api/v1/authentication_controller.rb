fcb5b1c384829e811a992b2f89d21d99f8f262cb
zeff96 <zeffo1975@gmail.com>
2023-10-11 04:19:49 +0300

    Add code refactor to include user in the api response

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -8,9 +8,15 @@ class Api::V1::AuthenticationController < ApplicationController
       if params[:user][:remember_me_token] == 'true'
         user.generate_remember_me_token
       end
-      render json: { message: 'Logged in successfully!', token: JsonWebToken.encode({ sub: user.id }) }, status: :ok
+      render json: { 
+        message: 'Logged in successfully!', 
+        user: user, 
+        token: JsonWebToken.encode({ sub: user.id }) 
+      }, status: :ok
     else
-      render json: { error: 'Incorrect email or password' }, status: :unauthorized
+      render json: { 
+        error: 'Incorrect email or password' 
+      }, status: :unauthorized
     end
   end
 end
