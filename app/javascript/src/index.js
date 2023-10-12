ed2c588b5944590823aec4238a2f3e6453a1276e
zeff96 <zeffo1975@gmail.com>
2023-10-12 16:55:32 +0300

    Add redux-persist wrapper around main app component

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -9,6 +10,8 @@ const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
