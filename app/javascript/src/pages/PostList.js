0820934ef1e8aa7b442551dc2f568a50fdd7467e
zeff96 <zeffo1975@gmail.com>
2023-10-13 17:22:19 +0300

    Add post sorting based on time created

--- app/javascript/src/pages/PostList.js
+++ app/javascript/src/pages/PostList.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useGetPostsQuery } from '../redux/api/posts/postSlice';
+import Post from '../components/Post';
 
 const PostList = () => {
+  const {
+    data: posts = [],
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsQuery();
+
+  const orderPosts = posts
+    .slice()
+    .sort((a, b) => a.created_at.localeCompare(b.created_at));
   return <div></div>;
 };
 
