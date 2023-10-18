399c0eaed50d5686bd18d8f8ea73832b5da1aac6
zeff96 <zeffo1975@gmail.com>
2023-10-18 18:51:32 +0300

    Move pop button to createpost modal

--- app/javascript/src/modals/CreatePostModal.js
+++ app/javascript/src/modals/CreatePostModal.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddPostForm from '../components/AddPostForm';
 
 const CreatePostModal = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div className='myModal' id='myModal'>
-      <div className='modal-header'>
-        <h2>Add new Post</h2>
-        <span>&#10060;</span>
+    <section>
+      <button type='button' className='btn-modal' id='myBtn'>
+        Add Post
+      </button>
+      <div className='myModal' id='myModal'>
+        <div className='modal-header'>
+          <h2>Add new Post</h2>
+          <span>&#10060;</span>
+        </div>
+        <div className='modal-body'>
+          <AddPostForm />
+        </div>
       </div>
-      <div className='modal-body'>
-        <AddPostForm />
-      </div>
-    </div>
+    </section>
   );
 };
 
