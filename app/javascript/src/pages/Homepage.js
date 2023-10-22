7026a3f25c1b583319b91f89d47621ce1cbd85e3
zeff96 <zeffo1975@gmail.com>
2023-10-22 16:12:02 +0300

    Add read auth state from store

--- app/javascript/src/pages/Homepage.js
+++ app/javascript/src/pages/Homepage.js
@@ -3,11 +3,13 @@ import './Homepage.css';
 import CreatePostModal from '../modals/CreatePostModal';
 import PostList from './PostList';
 import { selectAuthToken } from '../redux/features/authSlice';
+import { useAppSelector } from '../redux/features/hook';
 
 const Homepage = () => {
+  const token = useAppSelector(selectAuthToken);
   return (
     <>
-      {selectAuthToken && <CreatePostModal />}
+      {token && <CreatePostModal />}
       <PostList />
     </>
   );
