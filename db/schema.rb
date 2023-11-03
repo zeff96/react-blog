36624a6f7afec0c55b5430ebc4ee6816f8587439
zeff96 <zeffo1975@gmail.com>
2023-11-03 07:50:32 +0300

    Add notification migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_111145) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_03_044040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_111145) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "notification_type"
+    t.boolean "is_read", default: false, null: false
+    t.bigint "source_user_id"
+    t.bigint "source_post_id"
+    t.bigint "source_comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["source_comment_id"], name: "index_notifications_on_source_comment_id"
+    t.index ["source_post_id"], name: "index_notifications_on_source_post_id"
+    t.index ["source_user_id"], name: "index_notifications_on_source_user_id"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -58,5 +73,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_111145) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "notifications", "comments", column: "source_comment_id"
+  add_foreign_key "notifications", "posts", column: "source_post_id"
+  add_foreign_key "notifications", "users"
+  add_foreign_key "notifications", "users", column: "source_user_id"
   add_foreign_key "posts", "users"
 end
