74fe32685a0dce515579ee789ddb64f2e6c74616
zeff96 <zeffo1975@gmail.com>
2023-11-13 08:25:06 +0300

    Add onchange for textarea input element

--- app/javascript/src/modals/CommentModal.js
+++ app/javascript/src/modals/CommentModal.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useGetCommentsQuery, usePostCommentMutation } from '../redux/api/comment/comment'
 
 const CommentModal = (postId) => {
+  const[body, setBody] = useState('')
+
+  const{data, isLoading, isError, isSuccess, error} = useGetCommentsQuery(postId)
+  const[postComment] = usePostCommentMutation(postId)
+
   return (
     <div>
       <p>{Comment.body}</p>
       <hr />
       <form action="">
-        <textarea name="body" placeholder='write a comment' >
+        <textarea name="body" placeholder='write a comment' value={body} onChange={(e) => setBody(e.target.value)} >
           send
         </textarea>
       </form>
