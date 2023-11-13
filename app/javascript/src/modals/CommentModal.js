66a183fdbd1db09605cc4a29bfa9825c2b5b0455
zeff96 <zeffo1975@gmail.com>
2023-11-13 08:28:22 +0300

    Add function for adding new comment

--- app/javascript/src/modals/CommentModal.js
+++ app/javascript/src/modals/CommentModal.js
@@ -7,13 +7,21 @@ const CommentModal = (postId) => {
   const{data, isLoading, isError, isSuccess, error} = useGetCommentsQuery(postId)
   const[postComment] = usePostCommentMutation(postId)
 
+  const addPost = async() => {
+    try {
+      await postComment({body}).unwrap()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
       <p>{Comment.body}</p>
       <hr />
       <form action="">
         <textarea name="body" placeholder='write a comment' value={body} onChange={(e) => setBody(e.target.value)} >
-          send
+          <button type="button" onClick={addPost}>send</button>
         </textarea>
       </form>
     </div>
