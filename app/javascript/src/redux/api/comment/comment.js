eff16b9516cc1066878525f5b3bd62fa7b61be98
zeff96 <zeffo1975@gmail.com>
2023-11-13 08:17:26 +0300

    Add comment mutation

--- app/javascript/src/redux/api/comment/comment.js
+++ app/javascript/src/redux/api/comment/comment.js
@@ -4,8 +4,15 @@ const commentSlice = apiSlice.injectEndpoints({
   endpoints: build => ({
     getComments: build.query({
       query: (postId) => `posts/${postId}/comments`
-    }) 
+    }),
+    postComment: build.mutation({
+      query:(postId, body) => ({
+        url: `posts/${postId}/comments`,
+        method: 'POST',
+        body
+      })
+    })
   })
 })
 
-export const {useGetCommentsQuery} = commentSlice
+export const {useGetCommentsQuery, usePostCommentMutation} = commentSlice
